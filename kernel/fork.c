6ade02b334668ae4a5e6852a4aff372d8fc2375e
Hugh Dickins <hughd@google.com>
2017-09-03 18:57:03 -0700

    kaiser: stack map PAGE_SIZE at THREAD_SIZE-PAGE_SIZE
    Kaiser only needs to map one page of the stack; and
    kernel/fork.c did not build on powerpc (no __PAGE_KERNEL).
    It's all cleaner if linux/kaiser.h provides kaiser_map_thread_stack()
    and kaiser_unmap_thread_stack() wrappers around asm/kaiser.h's
    kaiser_add_mapping() and kaiser_remove_mapping().  And use
    linux/kaiser.h in init/main.c to avoid the #ifdefs there.
    
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Acked-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    # Conflicts:
    #	kernel/fork.c

--- kernel/fork.c
+++ kernel/fork.c
@@ -182,13 +182,11 @@ static unsigned long *alloc_thread_stack_node(struct task_struct *tsk,
 	return page ? page_address(page) : NULL;
 }
 
-extern void kaiser_remove_mapping(unsigned long start_addr, unsigned long size);
 static inline void free_thread_stack(unsigned long *stack)
 {
 	struct page *page = virt_to_page(stack);
-#ifdef CONFIG_KAISER
-	kaiser_remove_mapping((unsigned long)ti, THREAD_SIZE);
-#endif
+
+	kaiser_unmap_thread_stack(stack);
 	__free_kmem_pages(page, THREAD_SIZE_ORDER);
 }
 # else
@@ -373,7 +371,7 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 
 	tsk->stack = stack;
 
-	err= kaiser_add_mapping((unsigned long)tsk->stack, THREAD_SIZE, __PAGE_KERNEL);
+	err = kaiser_map_thread_stack(tsk->stack);
 	if (err)
 		goto free_stack;
 #ifdef CONFIG_SECCOMP
