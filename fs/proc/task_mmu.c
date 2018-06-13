f9e311ad98c1a6c3150a9711d42d008fe12dff69
freeza <freeza@gmail.com>
2018-06-13 15:15:16 -0700

    fixed fs/proc/task_mmu.c compile error

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -386,13 +386,12 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 
 		if (is_stack(priv, vma))
 			name = "[stack]";
-			goto done;
 		}
+
 		if (vma_get_anon_name(vma)) {
 			seq_pad(m, ' ');
 			seq_print_vma_name(m, vma);
 		}
-	}
 
 done:
 	if (name) {
